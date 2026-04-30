1f9625643086ebbe3ee606adc5dc1904d3a010e4
Javier Almiron <liofilizado@tutanota.com>
2026-04-30 15:31:21 +0000

    dia 4

--- models/staging/google/stg_google__budget.sql
+++ models/staging/google/stg_google__budget.sql
@@ -1,14 +1,17 @@
 {{ config(
-    materialized = 'table',
-    tag = ['silver']
-)}}
+    materialized='incremental',
+    unique_key = '_row',
+    incremental_strategy='merge'
+) }}
 
 
 with source as (
 
     select * 
     from {{ source('google', 'budget') }}
-
+    {% if is_incremental() %}
+        WHERE _row > (SELECT MAX(_row) FROM {{ this }})
+    {% endif %}
 ),
 
 renamed as (
@@ -19,7 +22,6 @@ renamed as (
         cast(month as date) as budget_month,
         product_id,
         _fivetran_synced
-
     from source
 
 )
---
+++ snapshots/snp_tmp.sql
@@ -0,0 +1,14 @@
+{% snapshot user_snapshot_s_timestamp %}
+
+{{
+    config(
+      target_schema='snapshots',
+      unique_key='DNI',
+      strategy='timestamp',
+      updated_at='FECHA_ALTA_SISTEMA',
+    )
+}}
+
+select * from {{ source('google', 'user') }} 
+
+{% endsnapshot %}
